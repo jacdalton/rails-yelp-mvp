0c59184addd1a3b057ada2f2eb15d5b8e8716f9e
Jacque Dalton <jacque.m.dalton@gmail.com>
2019-08-07 14:26:00 +0900

    Controller, router, and view setup for Restaurants (index, show, add/create)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating new restaurants..."
+
+5.times do 
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: Restaurant::CATEGORIES.sample
+  )
+  restaurant.save!
+end
+puts "Finished!"
